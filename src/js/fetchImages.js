975f9c538ec964b6479da79b122fe0a58b8582db
Vladyslav Polishchuk <105670228+vladpolischchuk@users.noreply.github.com>
2022-11-28 09:11:34 +0200

    Update fetchImages.js

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `https://pixabay.com/api/?key=31325871-0cde844d39520f6c11f9b55e1&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    `https://pixabay.com/api/?key=31657799-a6515ed5d1c3f7f5a7bc6e4d5=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
